3a08c2c9aa895d870bb411c7628e38390370dbb5
rsebokolodi023 <rsebokolodi023@student.wethinkcode.co.za>
2024-10-26 18:38:31 +0200

    flask

---
+++ App.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route for the Home Page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/')
+def about():
+    return render_template('booking.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
