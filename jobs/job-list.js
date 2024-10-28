56faa51af1e3aea00821ced8a454dbdaea152033
enhamuave023b <enhamuave023b@student.wethinkcode.co.za>
2024-10-28 23:14:53 +0200

    push push

---
+++ jobs/job-list.js
@@ -0,0 +1,24 @@
+const jCategory = [
+    {
+        index: 0,
+        image: "../images/ikhokha.png",
+        project: "Design branded graphics and posts for our social media campaigns.",
+        title: "Graphic Designer",
+        rate: "2400",
+        availability: "ongoing",
+        companyName: "iKhokha",
+        location: "JHB",
+        vacancy: "01",
+        duration: "1 Month",
+        description: "We’re looking for a creative graduate to design branded graphics and engaging posts for our social media campaigns. You’ll be responsible for creating eye-catching visuals that align with our brand’s look and feel, helping us connect with our audience and increase engagement across platforms.",
+        workplace: "Hybrid",
+        experience: "Fresh Graduate"
+    },
+    {
+        index: 1,
+        image: "../images/takealot.png",
+        project: "Optimize our website content for search engines to boost visibility.",
+        rate: "5000",
+        availability: "one-time" 
+    }
+]
